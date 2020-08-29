e21200fbeb4e50f7adbb81b2b018ea9264109782
Oscar-hub773 <69234357+Oscar-hub773@users.noreply.github.com>
2020-08-29 23:35:50 +0200

    Added-First-MVC-App

---
+++ RegisterEmployee_01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RegisterEmployee_01.Startup))]
+namespace RegisterEmployee_01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
